b510616fada29150a151e19cbace3e0678a536ea
Crysta <crysta@Nicks-MacBook-Pro.local>
2019-05-05 15:27:29 -0500

    Update Q objects

---
+++ js/main.js
@@ -0,0 +1,62 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+
+const aceVentura = {
+    quote: 'Alright then!',
+    correctVideoUrl: 'https://youtu.be/0tJGk4ofc18',
+    incorrectVideoUrl: 'https://youtu.be/WLQcBoKekmQ',
+    options: [
+      {
+        isCorrect: false,
+        movie: 'Talladega Nights'
+      },
+      {
+        isCorrect: false,
+        movie: 'Jurassic Park'
+      },
+      {
+        isCorrect: true,
+        movie: 'Ace Ventura'
+      },
+      {
+        isCorrect: false,
+        movie: 'Liar Liar'
+      }
+    ]
+  }
+
+  const starWars = {
+    quote: "Never tell me the odds"
+    correctVideoUrl: 'https://youtu.be/gRvu0yHoHy8?t=60',
+    incorrectVideoUrl: 'https://youtu.be/N1uTsqJVAeo',
+    options: [
+      {
+        isCorrect: true,
+        movie: 'Star Wars: The Empire Strikes Back'
+      },
+      {
+        isCorrect: false,
+        movie: 'Star Trek'
+      },
+      {
+        isCorrect: false,
+        movie: 'Good Will Hunting'
+      },
+      {
+        isCorrect: false,
+        movie: 'Legally Blond'
+      }
+    ]
+  }
