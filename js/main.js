64ad7efea4510b57e9fb1a7a508588ad53fdb581
Crysta <crysta@Nicks-MacBook-Pro.local>
2019-05-05 16:18:41 -0500

    update questions

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ const aceVentura = {
   }
 
   const starWars = {
-    quote: "Never tell me the odds"
+    quote: 'Never tell me the odds',
     correctVideoUrl: 'https://youtu.be/gRvu0yHoHy8?t=60',
     incorrectVideoUrl: 'https://youtu.be/N1uTsqJVAeo',
     options: [
@@ -57,6 +57,102 @@ const aceVentura = {
       {
         isCorrect: false,
         movie: 'Legally Blond'
+      }
+    ]
+  }
+
+  const lordOfTheRings = {
+    quote: 'Nobody tosses a dwarf',
+    correctVideoUrl: 'https://youtu.be/j4RowL_tZug',
+    incorrectVideoUrl: 'https://youtu.be/3Slv-p4Fato',
+    options: [
+      {
+        isCorrect: false,
+        movie: 'Snow White and the Seven Dwarfs'
+      },
+      {
+        isCorrect: false,
+        movie: 'The Chronicles of Narnia'
+      },
+      {
+        isCorrect: false,
+        movie: 'The Hobbit'
+      },
+      {
+        isCorrect: true,
+        movie: 'Lord of the Rings: The Fellowship of the Ring'
+      }
+    ]
+  }
+
+  const sandLot = {
+    quote: 'You never heard of the Sultan of Swat? The Titan of Terror? The Collosus of Clout?',
+    correctVideoUrl: 'https://youtu.be/VdLkYcnml4s?t=34',
+    incorrectVideoUrl: 'https://youtu.be/hxJPJ6JY0Pk?t=11',
+    options: [
+      {
+        isCorrect: false,
+        movie: 'Major League'
+      },
+      {
+        isCorrect: true,
+        movie: 'The Sandlot'
+      },
+      {
+        isCorrect: false,
+        movie: 'A League of Their Own'
+      },
+      {
+        isCorrect: false,
+        movie: 'The Avengers'
+      }
+    ]
+  }
+
+  const backToTheFuture = {
+    quote: '1.21 gigawatts? Great scott!',
+    correctVideoUrl: 'https://youtu.be/I5cYgRnfFDA?t=11',
+    incorrectVideoUrl: 'https://youtu.be/BWUP5QxdwPg',
+    options: [
+      {
+        isCorrect: false,
+        movie: 'Flubber'
+      },
+      {
+        isCorrect: false,
+        movie: 'Jurassic Park'
+      },
+      {
+        isCorrect: true,
+        movie: 'Back to the Future'
+      },
+      {
+        isCorrect: false,
+        movie: 'A Beautiful Mind'
+      }
+    ]
+  }
+
+  const homeAlone = {
+    quote: 'Why the hell are you dressed like a chicken?',
+    correctVideoUrl: 'https://youtu.be/VNWO9lfBg-s',
+    incorrectVideoUrl: 'https://youtu.be/Y-4-q3AiTcw',
+    options: [
+      {
+        isCorrect: false,
+        movie: 'Napoleon Dynamite'
+      },
+      {
+        isCorrect: false,
+        movie: 'Chicken Little'
+      },
+      {
+        isCorrect: false,
+        movie: 'Space Jam'
+      },
+      {
+        isCorrect: true,
+        movie: 'Home Alone'
       }
     ]
   }
